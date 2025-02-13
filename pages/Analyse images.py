1f931ab834a70f9b4ba6f10ec560c8839c13fcdc
Kanoshiko <raidelet@gmail.com>
2025-02-13 11:55:38 +0100

    Create Analyse images.py

---
+++ pages/Analyse images.py
@@ -0,0 +1,32 @@
+from mistralai import Mistral
+import streamlit as st
+
+client = Mistral(api_key="el3j7sMe3Dq86EOj6uIjTnnVeqZKDFrq")
+
+# Titre
+st.title("Analyse une image avec Mistral AI")
+
+prompt = st.text_input("Entrez l'url de l'image")
+
+# Analyse d'image unique
+if st.button("Envoyer") :
+  chat_response = client.chat.complete(
+      model = "pixtral-12b-2409",
+      messages= [
+      {
+          "role": "user",
+          "content": [
+              {
+                  "type": "text",
+                  "text": "Que contient cette image ?"
+              },
+              {
+                  "type": "image_url",
+                  "image_url": prompt
+              }
+          ]
+      }
+  ]
+  )
+
+  st.write(chat_response.choices[0].message.content)
